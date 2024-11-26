945537a1ed8b2902f63b528a60b8fe30d36355be
AhmedZidan399 <ahmed.zidan.new10@gmail.com>
2024-11-26 17:01:40 +0200

    Add project files.

---
+++ PlantoPia/Models/PlantOpiaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlantoPia.Models
+{
+	public class PlantOpiaContext : IdentityDbContext<Users>
+	{
+		public PlantOpiaContext() : base() { }
+
+		public PlantOpiaContext(DbContextOptions<PlantOpiaContext> options) : base(options) { }
+		
+		public DbSet<Users> users { get; set; }
+	}
+}
---
+++ PlantoPia/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlantoPia.Models
+{
+	public class Users:IdentityUser
+	{
+		
+       // public int Id { get; set; }
+		
+		[MinLength(20,ErrorMessage ="Name Should be more Than 3 Letters")]
+		public string name { get; set; }
+		
+		public string email { get; set; }
+		[RegularExpression("Male | Female",ErrorMessage ="Gender Should be Male or Female")]
+        public string Gender { get; set; }
+        public string password { get; set; }
+		public string phone { get; set; }
+
+        public String ProfileImage { get; set; }
+    }
+}
